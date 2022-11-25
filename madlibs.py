917c471876eea912ceae99aebb84b0e3e2c76c91
Sandrita <sandramilenagarciaa@gmail.com>
2022-11-24 19:12:32 -0800

    refactor code

--- madlibs.py
+++ madlibs.py
@@ -4,8 +4,6 @@ from random import choice
 
 from flask import Flask, render_template, request
 
-# "__name__" is a special Python variable for the name of the current module.
-# Flask wants to know this to know what any imported things are relative to.
 app = Flask(__name__)
 
 AWESOMENESS = [
@@ -37,16 +35,10 @@ def start_here():
 def greet_person():
     """Greet user with compliment."""
 
-    player = request.args.get("person")
-
-    compliment = choice(AWESOMENESS)
-
-    return render_template("compliment.html", person=player, compliment=compliment)
+    return render_template("compliment.html")
 
 @app.route("/game")
 def show_madlib_forme():
- 
-    # tester code 
     
     play_game = request.args.get("playGame")
     
@@ -56,28 +48,16 @@ def show_madlib_forme():
     else:
         return render_template("goodbye.html")
 
-    # answer = request.args.get()
-
 
 @app.route("/madlib")
 def madlib():
     colors = request.args.get("color")
-    # colors = "red"
     nouns = request.args.get("noun")
     persons = request.args.get("person")
     adjectives = request.args.get("adjective")
     return render_template("madlib.html", color = colors, noun = nouns, person = persons, adjective= adjectives)
 
-@app.route("/goodbye")
-def say_goodbye():
-    """Say hello to user."""
-
-    return render_template("goodbye.html")
-
-
 
 if __name__ == "__main__":
-    # Setting debug=True gives us error messages in the browser and also
-    # "reloads" our web app if we change the code.
 
     app.run(debug=True, host="0.0.0.0", port=5003 )
